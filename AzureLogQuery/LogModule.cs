4a4e83260356da1b101745c5f48e32da28f04edf
Christopher Eyre <chriseyre2000@gmail.com>
2014-03-19 22:01:09 +0000

    Extracted common method. Added explicit paths.

--- AzureLogQuery/LogModule.cs
+++ AzureLogQuery/LogModule.cs
@@ -10,24 +10,42 @@ namespace AzureLogQuery
     {
         public LogModule()
         {
+            Get["Environment/{Environment}/Period/{Period}/Level/{Level}"] = x =>
+            {
+                LogModel[] logModel = CreateLogModel(x);
+                return Response.AsJson(logModel);
+            };
+
+            
             Get["/{Environment}/{Period}/{Level}"] = x => 
             {
-                string environment = x.Environment;
-                LogPeriod period = Enum.Parse(typeof(LogPeriod), x.Period, ignoreCase: true);
-                int level = int.Parse(x.Level); 
-                LogQuery query = new LogQuery();
-                return Response.AsJson(query.Execute(environment, period, level));
+                LogModel[] logModel = CreateLogModel(x);
+                return Response.AsJson(logModel);
             };
+
+            Get["View/Environment/{Environment}/Period/{Period}/Level/{Level}"] = x =>
+            {
+                LogModel[] logModel = CreateLogModel(x);
+                return logModel;
+            };
+
 
             Get["View/{Environment}/{Period}/{Level}"] = x =>
             {
-                string environment = x.Environment;
-                LogPeriod period = Enum.Parse(typeof(LogPeriod), x.Period, ignoreCase: true);
-                int level = int.Parse(x.Level);
-                LogQuery query = new LogQuery();
-                return query.Execute(environment, period, level);
+                LogModel[] logModel = CreateLogModel(x);
+                return logModel;
             };
 
+        }
+
+        private static LogModel[] CreateLogModel(dynamic x)
+        {
+            string environment = x.Environment;
+            LogPeriod period = (LogPeriod)Enum.Parse(typeof(LogPeriod), x.Period, ignoreCase: true);
+            int level = int.Parse(x.Level);
+            LogQuery query = new LogQuery();
+            var logModel = query.Execute(environment, period, level);
+            return logModel;
         }
     }
 }
